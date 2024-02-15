7daf164d6e65330167d152317ca8a55ae53deca3
Rom-Freedom <romeo.dark@mail.ru>
2024-02-16 00:15:52 +0300

    Complete the project

--- js/script.js
+++ js/script.js
@@ -1,22 +1,33 @@
 let inputBx = document.querySelector('#inputBx');
 let list = document.querySelector('#list');
 
+//Add an inputed entry by Enter click
 inputBx.addEventListener('keyup', function(event) {
-    if (event.key = 'Enter') {
+    if (event.key == 'Enter') {
         addItem(this.value)
         this.value = ''
     }
 })
 
+//Create an li-element with innerHTML property
 let addItem = (inputBx) => {
     let listItem = document.createElement('li');
     listItem.innerHTML = `${inputBx}<i></i>`;
 
-    listItem.addEventListener('click', function() {
-        this.classList.toggle('done');
-    })
+   
+//Switch a completed task
+listItem.addEventListener('click', function() {
+    this.classList.toggle('done');
+})
 
-    list.appendChild(listItem);
+    //Function to delete a task
+    listItem.querySelector('i').addEventListener('click',
+    function() {
+        listItem.remove();
+})
+
+//Add item from the input form
+list.appendChild(listItem);
 }
 
 
